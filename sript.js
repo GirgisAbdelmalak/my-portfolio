ba4e0a6f2748e3e3217e02d1131bb64e28a638c1
gergeskeliny <gergeskeliny60@gmail.com>
2025-05-10 21:42:03 +0300

    improved version of my portfolio

---
+++ sript.js
@@ -0,0 +1,21 @@
+const data = document.getElementById("data");
+document.getElementById("menuBtn").addEventListener("click", function () {
+    if(data.classList.contains("move-down")){
+        data.classList.remove("move-down")
+        data.classList.add("move-up")
+    }
+    else{
+        data.classList.remove("move-up")
+        data.classList.add("move-down")
+    }
+});
+
+document.getElementById('downloadResumeBtn').addEventListener('click', function() {
+    // Create a temporary link
+    const link = document.createElement('a');
+    link.href = 'Assets/Gerges_CV.pdf';
+    link.download = 'Gerges CV.pdf'; // Optional: set filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
